bc011ae4bd64dea0935b6fa56a62dc4a52717acf
iis2h <xsahss6@gmail.com>
2024-03-31 22:47:33 +0300

    NEW VERSION

--- README.md
+++ README.md
@@ -1,39 +1,117 @@
 # RobotSeeker
-![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/iis2h/RobotSeeker?label=version)
-![GitHub top language](https://img.shields.io/github/languages/top/iis2h/RobotSeeker?color=15E245)
+
+<p>
+  <a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/github/license/iis2h/RobotSeeker.svg"></a>
+  <a href="https://github.com/iis2h/RobotSeeker/releases"><img src="https://img.shields.io/github/v/tag/iis2h/RobotSeeker?label=version"></a>
+  <a href="https://www.python.org"><img src="https://img.shields.io/github/languages/top/iis2h/RobotSeeker?color=15E245"></a>
+  <a href="https://github.com/iis2h/RobotSeeker/issues"><img src="https://img.shields.io/badge/contributes-welcome-blue"></a>
+</p>
+
+<p>
+  <a href="#features">Features</a> •
+  <a href="#installation">Installation</a> •
+  <a href="#usage">Usage</a> •
+  <a href="#status">Status</a> •
+  <a href="#flags">Flags</a> •
+  <a href="#output">Output</a> •
+  <a href="https://github.com/iis2h/RobotSeeker/main/CHANGELOG.txt">CHANGELOG</a>
+</p>
 
 
+RobotSeeker is fast and reliable python tool that grabs `robots.txt` files from a bunch of subdomains
 
-RobotSeeker is a tool that grabs `robots.txt` files from a bunch of subdomains and does the following stuff:
+#### Why is grabbing robots.txt useful ?
+`robots.txt` can include sensitive information and endpoints intended to be hidden
 
-* Generate a wordlist from the words in all the `robots.txt` files. Helpful for fuzzing the same target.
-* Map the directories from each `robots.txt` file with its URL.
-* Write all the working URLs into one file.
+## Features
 
-## Why is grabbing robots.txt useful ?
-`robots.txt` can include sensitive information and directories intended to be hidden.
+* Write all found `robots.txt` URLs into one file
+* Generate a wordlist from the words in all the `robots.txt` files. Helpful for fuzzing the same target
+* Map the endpoints from each `robots.txt` with its subdomain
 
-## Installation & Usage
-#### Installation
-``` bash
-git clone https://github.com/iis2h/RobotSeeker.git
-cd RobotSeeker
+## Installation
+
+```bash
+git clone https://github.com/iis2h/RobotSeeker.git &&
+cd RobotSeeker &&
 pip3 install -r requirements.txt
 ```
-#### Usage
-``` bash
-robotseeker.py -f Subdomains.txt -mgv -r 3
-````
-* `-f` : path to the file
-* `-m` : Map the content of `robots.txt` to its corresponding URL
-* `-g` : Generate a wordlist
-* `-v` : Enable verbose output
-* `-r` : Requests per second "Default is 3"
-* `-h` : Help menu
-* `--version` : Display version
 
-
-## CHANGELOG
-| VERSION | UPDATE |
+## Usage
+### Flags
+| Flag | Description
 |---|---|
-| 1.0.0 | Initial Release |
+| `-h` | Help menu |
+| `-f` | Path to the input file |
+| `-m` | Map the content of robots.txt to its corresponding URL |
+| `-g` | Generate a wordlist |
+| `-r` | Requests per second (Default is 3) *Increasing the rate might affect the result* |
+| `-v` | Enable verbose output |
+| `-q` | Quite Mode (no banner) |
+| `--version` | Display version |
+
+### Status
+
+| Status | Description |
+|---|---|
+| `[Satus Code]` | HTTP status codes (200, 404, 502 ...) |
+| `BLANK` | Either an empty page or a Soft 404 page |
+| `ERROR` | Error when trying to connect |
+
+*normal*
+```bash
+pyhton3 robotseeker.py -f subdomains.txt -m -g
+```
+```bash
+  ___  ___  ___  ___ _____ ___ ___ ___ _  _____ ___
+ | _ \/ _ \| _ )/ _ |_   _/ __| __| __| |/ | __| _ \
+ |   | (_) | _ | (_) || | \__ | _|| _|| ' <| _||   /
+ |_|_\\___/|___/\___/ |_| |___|___|___|_|\_|___|_|_\
+
+ Crafted with Passion by iis2h aka Frenzy
+
+[200]: http://sub.example-1.com/robots.txt
+[200]: http://sub.example-2.com/robots.txt
+[200]: http://sub.example-3.com/robots.txt
+[200]: http://sub.example-4.com/robots.txt
+[200]: http://sub.example-5.com/robots.txt
+```
+
+*verbos*
+```bash
+pyhton3 robotseeker.py -f subdomains.txt -m -g -v
+```
+```bash
+  ___  ___  ___  ___ _____ ___ ___ ___ _  _____ ___
+ | _ \/ _ \| _ )/ _ |_   _/ __| __| __| |/ | __| _ \
+ |   | (_) | _ | (_) || | \__ | _|| _|| ' <| _||   /
+ |_|_\\___/|___/\___/ |_| |___|___|___|_|\_|___|_|_\
+
+ Crafted with Passion by iis2h aka Frenzy
+
+[404]: https://sub.example-1.com/robots.txt
+[200]: https://sub.example-2.com/robots.txt
+[ERROR]: Cannot connect to https://sub.example-3.com
+[502]: https://sub.example-4.com/robots.txt
+[BLANK]: https://sub.example-5.com/robots.txt
+```
+
+
+### Output
+| File | Description | Created When |
+|---|---|---|
+| valid | Valid URLs | Automatically |
+| wordlist | Generated Wordlist | When using `-g` flag |
+| mapped | Endpoints Mapped to its Subdomain | When using `-m` flag |
+
+*valid*
+<p><img width="500" alt="valid" src="https://github.com/iis2h/RobotSeeker/assets/43062742/4117d68f-21c3-48b9-abac-2370784f2fde"></p>
+
+*wordlist*
+<p><img width="500" alt="wordlist" src="https://github.com/iis2h/RobotSeeker/assets/43062742/061146ee-5fa3-4953-9803-09863e14bd2c"></p>
+
+*mapped*
+<p><img width="500" alt="mapped" src="https://github.com/iis2h/RobotSeeker/assets/43062742/db0f385f-583f-495d-a8c0-6a6206411b9f"></p>
+
+---
+> Your stars greatly support our project ⭐ 
--- robotseeker.py
+++ robotseeker.py
@@ -1,36 +1,53 @@
+#!/bin/python3
+
 import aiohttp
 import asyncio
 import argparse
 import re
 import os
 
+# Argumetns
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, metavar='File', help='Path to the file')
 parser.add_argument('-m', action='store_true', help='Map the content of "robots.txt" to its corresponding URL')
 parser.add_argument('-g', action='store_true', help="Generate a wordlist")
-parser.add_argument('-v', action='store_true', help="Enable verbose output")
-parser.add_argument('-r', type=int, metavar='Rate Limit', default=3, help='Requests per second "Default is 3"')
-parser.add_argument('--version', action='store_true', help="Display version")
+parser.add_argument('-r', type=int, metavar='Rate Limit', default=3, help='Requests per second (Default is 3)')
+parser.add_argument('-v', action='store_true', help="Verbose - Display all status codes")
+parser.add_argument('-q', action='store_false', help='Don\'t display banner')
+parser.add_argument('--version', action='store_true', help="Version")
 
+# ANSI Colors
 GREEN = '\033[32m'
 YELLOW = '\033[33m'
 RED = '\033[31m'
+BLUE = '\033[34m'
 BOLD = '\033[1m'
 RESET = '\033[0m'
 
-print('\nProcessing ... \n')
+# Banner
+def banner():
+    banner = r'''
+  ___  ___  ___  ___ _____ ___ ___ ___ _  _____ ___ 
+ | _ \/ _ \| _ )/ _ |_   _/ __| __| __| |/ | __| _ \
+ |   | (_) | _ | (_) || | \__ | _|| _|| ' <| _||   /
+ |_|_\\___/|___/\___/ |_| |___|___|___|_|\_|___|_|_\
+'''
+    print(banner)
+    print(f" Crafted with Passion by iis2h aka {BLUE}Frenzy{RESET}\n")
 
-def UrlFilter(inputData):
-    with open(inputData, 'r') as f:
+# Filter input data
+def urls_filter(input_data):
+    with open(input_data, 'r') as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
-        urls = set()
+        urls = []
         for url in lines:
-            if url.startswith('http'):
+            if url.startswith('http') and '*' not in url:
                 pattern = re.compile(r'(https?://[^/?]+).*')
-                urls.add(pattern.sub(r'\1', url) + '/robots.txt')
-        return urls
+                urls.append(pattern.sub(r'\1', url) + '/robots.txt')
+        return sorted(set(urls))
 
-def UniqFilename(base):
+# Create unique output file names
+def unique_filename(base):
     file_number = 1
     file_name = f'{base}_{file_number}.txt'
     while os.path.exists(file_name):
@@ -38,76 +55,149 @@ def UniqFilename(base):
         file_name = f'{base}_{file_number}.txt'
     return file_name
 
-def WordlistGenerator(text):
-    words_to_remove = ['user-agent','disallow', 'allow', 'user-agentadsbot-google', 
-                       'user-agentadsbot-google-mobile', 'user-agentgooglebot-image', 
-                       'googlebot-image', 'www.robotstxt.org'] 
-    symblos_to_remove = {ord('$') : None,  ord('*') : None, ord('(') : None, ord(')') : None, 
-                         ord('#') : None, ord(':') : None, ord('!') : None, ord('"') : None} 
-    pattern = r'^-*|-$|\.$'
-    lines = [line.replace('/', ' ').replace('&', ' ').replace('=', ' ').replace('?', ' ').replace(',', ' ') for line in text]
-    init = [re.sub(pattern , '', item.translate(symblos_to_remove)) for value in lines for item in value.split(' ')]
-    cleaned = set(str(word) for word in init if word.lower() not in words_to_remove and word != '')
-    return '\n'.join(cleaned)
+# Generate a wordlist
+def wordlist_generator(content):
+    words_to_remove = ['user-ano_domainst','disallow', 'allow', 'crawl-delay' , 'googlebot', 
+                       'user-ano_domainstadsbot-google', 'user-ano_domainstadsbot-google-mobile', 
+                       'user-ano_domainstgooglebot-image', 'googlebot-image', 'www.robotstxt.org', 
+                       'googlebot-news', 'robotstxt', 'robotstxt.html']
+    symbols_to_remove = {ord('$') : None,  ord('*') : None, ord('(') : None, ord(')') : None, 
+                         ord('#') : None, ord(':') : None, ord('!') : None, ord('"') : None,
+                         ord('%') : None}   
+    # Regex Patterns
+    pattern = r'^-*|-$|\.$|^\.\w+$'
+    domain_pattern = re.compile(r'(\w+)\.(\w+)\.(\w+)')    
+    # Replace symbols with spaces
+    lines = [line.replace('/', ' ').replace('&', ' ').replace('=', ' ')
+             .replace('?', ' ').replace(',', ' ') 
+             for line in content]   
+    # Translate over {symbols_to_remove}
+    no_symbols = [re.sub(pattern , '', item.translate(symbols_to_remove)) 
+            for value in lines 
+            for item in value.split(' ')]   
+    # Seperate 'www.example.com' into three words (www , example , com)
+    no_domains = [item for word in no_symbols 
+           for item in (domain_pattern.match(word).groups() 
+            if domain_pattern.match(word) else [word])]  
+    # Final Pure Wordlist
+    wordlist = {str(word).replace('\n', '').replace('\t', '').replace('\r','').strip() 
+                for word in no_domains 
+                if word.lower() not in words_to_remove and word}  
+    return '\n'.join(sorted(set(wordlist)))
 
-def DirMapper(url, text):
-    pattern = re.compile(r'(https?://[^/?]+).*')
-    lower_text = text.lower()
-    if lower_text.startswith('sitemap:'):
-        return lower_text.split(':', 1)[1].strip()
-    elif '/' in text and '#' not in text:
-        return  str(pattern.sub(r'\1', str(url))) + str(text.replace('Disallow:', '').replace('Allow:', '').strip())
+# Mapping URLs to the endpoints specified in its robots.txt file
+def mapper(url, content):
+    # Regex Patterns
+    url_pattern = re.compile(r'(https?://[^/?]+).*')
+    content_pattern = re.compile(r'(?mi)^(.*)(Disallow|Allow)[:]?\s*')
+    symbol_pattern = re.compile(r'\*|\$')   
+    lower_text = content.lower()
+    # Remove words "Sitemap:", "Host:" and return their value
+    if lower_text.startswith(('sitemap:', 'host:')):
+        return content.split(':', 1)[1].strip()
+    # Discard "Crawl-Delay:" and its value
+    elif lower_text.startswith('crawl-delay:'):
+        return None
+    # Discard Comments
+    elif lower_text.startswith('#'):
+        return None
+    # Dealing with endpoints
+    elif '/' in lower_text:
+        endpoint = re.sub(content_pattern, '', str(content))
+        endpoint = re.sub(symbol_pattern, '', str(endpoint))
+        # If the value is a complete URL, add it directly
+        if endpoint.startswith(('http://', 'https://')):
+            return endpoint.strip()
+        else:
+            # Append endpoints to the base domain
+            base_domain = str(url_pattern.sub(r'\1', str(url)))
+            return base_domain + endpoint.strip()
 
 async def fetch(session, url, semaphore):
     try:
         async with semaphore:
+            # Requesting method and SSL check is disabled
             async with session.get(url, ssl=False) as response:
-                return await response.text(), response.url, response.status, response.headers.get('Content-Type', '')
+                # Check for 'text/plain' in the 'Content-Type' header to identify robots.txt only and discard Soft 404 pages or Blank Pages
+                if response.status == 200 and 'text/plain' in response.headers.get('Content-Type', ''):
+                    print(f'[{GREEN}{response.status}{RESET}]: {url}')
+                    # Return a tuple containing the response body and URL
+                    return await response.text(), response.url
+                elif args.v and response.status != 200:
+                    if response.status == 404:
+                        print(f'[{RED}{response.status}{RESET}]: {url}')
+                    else:
+                        print(f'[{YELLOW}{response.status}{RESET}]: {url}')
+                elif args.v:
+                    print(f'[{BOLD}BLANK{RESET}]: {url}')
+                # Don't return either Body or URL
+                return None, None
     except Exception as e:
-        print(f'[{BOLD}{YELLOW}Error{RESET}] : {e}')
+        if args.v:
+            print(f'[{BOLD}{YELLOW}ERROR{RESET}]: {e}')
+            # Don't return either Body or URL
+            return None, None
 
 async def main():
+    # If quiet mode is enabled don't display banner
+    if args.q:
+        banner()
+    else:
+        print('\n')
+    # Rate Limit function using Semaphore
     semaphore = asyncio.Semaphore(args.r)
     async with aiohttp.ClientSession() as session:
-        basic_urls = UrlFilter(args.f)
-        tasks = [fetch(session, url, semaphore) for url in basic_urls]
-        valid = set()
-        texts = []
-        mapped = set()
+        base_urls = urls_filter(args.f)
+        tasks = [fetch(session, url, semaphore) for url in base_urls]
+        # List contains valid URLs
+        valid = []
+        # List contains everything inside robots.txt files
+        content = []
+        # List contins URLs mapped to the endpoints
+        mapped = []
         responses = await asyncio.gather(*tasks, return_exceptions=True)
         for response in responses:
-            if isinstance(response, tuple) and len(response) == 4:
-                text, url, status, header = response
-                if status == 200 and 'text/plain' in header:
-                    print(f'[{BOLD}{GREEN}{status}{RESET}]: {url}')
-                    valid.add(str(url))
+            # Check if the response exists and it's a tuple with two elements
+            if response and len(response) == 2:
+                text, url = response
+                # Check if the text is not 'None' or Empty
+                if text:
+                    # If text is there that's mean the url status code is [200 OK], not Soft 404 or Empty Page
+                    valid.append(str(url))
                     text_lines = text.splitlines()
-                    texts.extend(text_lines)
+                    content.extend(text_lines)
+                    # If mapping is enabled
                     if args.m:
+                        # Take every item in 'text_lines' list
                         for item in text_lines:
-                            mapped_url = DirMapper(url, item)
+                            # And apply mapper function to it
+                            mapped_url = mapper(url, item)
+                            # Ensure the result not Empty and then add it to 'mapped' list
                             if mapped_url:
-                                mapped.add(mapped_url)
-                elif args.v and status != 200:
-                    print(f'[{BOLD}{RED}{status}{RESET}]: {url}')        
-        
-        return valid, texts, sorted(mapped)
+                                mapped.append(mapped_url)                    
+        return sorted(set(valid)), content, sorted(set(mapped))
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    valid, texts, mapped = asyncio.run(main())
-    if args.f:
-        filename = UniqFilename('robots_valid')
-        with open(filename, 'x') as v:
-            v.write('\n'.join(valid))
-    if args.g:
-        filename = UniqFilename('robots_wordlist')
-        with open(filename, 'x') as w:
-            w.write(WordlistGenerator(texts))
-    if args.m:
-        filename = UniqFilename('robots_mapped')
-        with open(filename, 'x') as w:
-            w.write('\n'.join(mapped))
-    if args.version:
-        version = '1.0.0'
-        print(f'Version : {version}')
+    try:
+        if not args.f:
+            print(f'\n Please add an input file using -f flag\n')
+        else:
+            valid, content, mapped = asyncio.run(main())
+        if args.f:
+            filename = unique_filename('valid')
+            with open(filename, 'x') as v:
+                v.write('\n'.join(valid))
+        if args.g:
+            filename = unique_filename('wordlist')
+            with open(filename, 'x') as w:
+                w.write(wordlist_generator(content))
+        if args.m:
+            filename = unique_filename('mapped')
+            with open(filename, 'x') as w:
+                w.write('\n'.join(mapped))
+        if args.version:
+            version = '2.0.0'
+            print(f'Version : {version}')
+    except KeyboardInterrupt:
+        print(f' \nKeyboard Interrupt\n')
